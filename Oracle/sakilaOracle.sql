66bd77de1b5998247486e6211061af1be98a191f
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2019-11-29 11:54:57 +0000

    Update sakilaOracle.sql

--- Oracle/sakilaOracle.sql
+++ Oracle/sakilaOracle.sql
@@ -326,6 +326,33 @@ CREATE TABLE film_text (
   CONSTRAINT pk_film_text PRIMARY KEY (film_id)
 );
 
+-- Criação dos triggers adicionais da tabela film para a filmtext.
+CREATE OR REPLACE TRIGGER ins_film
+AFTER INSERT ON film FOR EACH ROW
+BEGIN
+    INSERT INTO film_text (film_id, title, description)
+        VALUES (:NEW.film_id, :NEW.title, :NEW.description);
+END;
+
+CREATE OR REPLACE TRIGGER upd_film
+AFTER UPDATE ON film FOR EACH ROW
+BEGIN
+    IF (:OLD.title != :NEW.title) or (:OLD.description != :OLD.description)
+    THEN
+        UPDATE film_text
+            SET title=:NEW.title,
+                description=:NEW.description,
+                film_id=:NEW.film_id
+        WHERE film_id=:OLD.film_id;
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER del_film
+AFTER DELETE ON film FOR EACH ROW
+BEGIN
+    DELETE FROM film_text WHERE film_id = :OLD.film_id;
+END;
+
 -- Estrutura da tabela inventory
 CREATE TABLE inventory (
     inventory_id INT NOT NULL,
@@ -477,7 +504,7 @@ CREATE TABLE rental (
 );
 
 -- Criação do indíce adicional presente no rental.
-CREATE UNIQUE INDEX   rental_I  ON rental (rental_date,inventory_id,customer_id);
+CREATE UNIQUE INDEX rental_I ON rental (rental_date,inventory_id,customer_id);
 
 -- Criação do "auto-increment" do category através da sequência e do seu trigger.
 CREATE SEQUENCE rental_sq
